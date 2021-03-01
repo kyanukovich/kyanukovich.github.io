fb3e63285702713c5de373e0f4504302f4d1b893
kyanukovich <46454891+kyanukovich@users.noreply.github.com>
2021-03-01 13:08:10 +0200

    Minor updates

---
+++ dz1/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $(".navbar__burger").click(function (event) {
+    $(".menu, .navbar__burger").toggleClass("open");
+    $("body").toggleClass("lock");
+  });
+});
